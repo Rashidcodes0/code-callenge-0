6c6165a5773e4169215973115310dec875f4f6ce
Abdirashidnoor <abdirashid16112@gmail.com>
2023-03-21 11:46:09 +0300

    final commit

--- index.js
+++ index.js
@@ -53,3 +53,55 @@ function calculate() {
         }
     }
 }
+
+// Challenge 3: Net Salary Calculator
+
+// Calculate PAYE deductions based on gross pay
+function calculatePaye(grossPay) {
+    let taxPercentage = 0;
+    if (grossPay <= 24000) {
+        taxPercentage = 10;
+    } else if (grossPay <= 32333) {
+        taxPercentage = 25;
+    } else {
+        taxPercentage = 30;
+    }
+    const paye = grossPay * taxPercentage / 100;
+    const personalRelief = 2400; // Personal relief is a fixed amount
+    return paye - personalRelief;
+}
+
+// Calculate NHIF deductions based on gross pay
+function calculateNhifDeductions(grossPay) {
+    let nhifDeduction = 0;
+    const nhifBrackets = [
+        { max: 5999, amount: 150 },
+        { max: 7999, amount: 300 },
+        { max: 11999, amount: 400 },
+        { max: 14999, amount: 500 },
+        { max: 19999, amount: 600 },
+        { max: 24999, amount: 750 },
+        { max: 29999, amount: 850 },
+        { max: 34999, amount: 900 },
+        { max: 39999, amount: 950 },
+        { max: 44999, amount: 1000 },
+        { max: 49999, amount: 1100 },
+        { max: 59999, amount: 1200 },
+        { max: 69999, amount: 1300 },
+        { max: 79999, amount: 1400 },
+        { max: 89999, amount: 1500 },
+        { max: 99999, amount: 1600 },
+        { max: Infinity, amount: 1700 }
+    ];
+    const bracket = nhifBrackets.find(bracket => grossPay <= bracket.max); // Find the first NHIF bracket that matches the gross pay
+    if (bracket) {
+        nhifDeduction = bracket.amount;
+    }
+    return nhifDeduction;
+}
+
+// Calculate NSSF deductions based on gross pay
+function calculateNssfDeductions(grossPay) {
+    const nssfDeduction = grossPay * 5 / 100; // NSSF deduction is 5% of gross pay
+    return nssfDeduction > 400 ? 400 : nssfDeduction; // NSSF deduction is capped at Ksh 400
+}
